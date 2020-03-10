c326a1caa4d5b419d02f6967a6cfe257b3b1613f
Peter Rose <peterroseq1@gmail.com>
2020-03-10 18:22:28 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0 
+  
+  while i < array.length 
+   yield array[i]
+  end
+  array
+end
+
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
+  name.split(" ").first 
+end 
 
 
