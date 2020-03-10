e5c6b73f37fd0a71477c325016d3d4766f3ca189
Peter Rose <peterroseq1@gmail.com>
2020-03-10 20:28:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,17 +14,17 @@ my_collect(array) do |name|
 end
 
 
-def my_collect(languages)
+def my_collect(collection)
   i = 0 
-  collection = []
-  while i < languages.length 
-   collection << yield languages[i]
+  languages = []
+  while i < collection.length 
+   languages << yield collection[i]
    i +=1 
   end
   collection
 end
 
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(languages) do |lang|
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
   lang.upcase
 end
