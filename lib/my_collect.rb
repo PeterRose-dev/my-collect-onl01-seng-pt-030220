08bd9dcefbaef3aa3bf96d8da195c5b12e138e55
Peter Rose <peterroseq1@gmail.com>
2020-03-10 20:32:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,30 @@
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+array = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
 
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+array = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(array) do |lang|
+  lang.upcase
+end
