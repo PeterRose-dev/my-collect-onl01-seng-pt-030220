f423be5eec09208b58a4bf9fe4a3a84866890981
Peter Rose <peterroseq1@gmail.com>
2020-03-10 20:01:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,4 +11,16 @@ my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
   name.split(" ").first 
 end 
 
+def my_collect(languages)
+  i = 0 
+  
+  while i < languages.length 
+   yield languages[i]
+  end
+  languages
+end
+
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
+  name.split(" ").first 
+end 
 
